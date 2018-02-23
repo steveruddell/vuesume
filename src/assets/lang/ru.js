f48eac0bc5dbaca93e7064c6f2ee0eac548f1b94
Steve Ruddell <steveruddell@me.com>
2018-02-23 14:09:39 -0500

    add Russian Language translations

---
+++ src/assets/lang/ru.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const ru = {
+    message: {
+      location: 'место',
+      experience: 'опыт',
+      education: 'образование',
+      skills: 'навыки',
+      languages: 'языки',
+      portfolio: 'портфолио',
+      honors: 'почести',
+      about: 'около'
+    }
+  }
+  export default ru
