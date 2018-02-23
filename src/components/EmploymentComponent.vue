ce0f47baab871b84c1b5b35a4b137e32aa627ad3
Steve Ruddell <steveruddell@me.com>
2018-02-23 16:16:46 -0500

    add employment activities

--- src/components/EmploymentComponent.vue
+++ src/components/EmploymentComponent.vue
@@ -3,9 +3,28 @@
 <script>
 import TimelineView from './TimelineComponent'
 export default {
+  computed: {
+    employment () {
+      for (var item in this.$store.getters.getSection('employment').items) {
+        this.collapse.push(false)
+        item = item + 0
+      }
+      return this.$store.getters.getSection('employment')
+    }
+  },
   data () {
     return {
-      employment: this.$store.getters.getSection('employment')
+      collapse: []
+    }
+  },
+  methods: {
+    toggleActivities (index) {
+      this.$set(this.collapse, index, !this.collapse[index])
+      console.log(this.collapse)
+    },
+    isVisible (index) {
+      console.log('vis?', this.collapse[index])
+      return this.collapse[index]
     }
   },
   components: {'TimelineView': TimelineView}
@@ -13,5 +32,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
